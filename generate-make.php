05136573484cdfc5bfc1c2d598dd170ea323a61a
Choon-Siang Lai <46977+Jeffrey04@users.noreply.github.com>
2014-02-04 22:46:47 +0800

    initial state

---
+++ generate-make.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php
+    // read the config
+?>
+
+all: .vimrc .vim <?php // todo install-$plugins ?>
+
+<?php
+    //todo: make update-$pluginname
+?>
+
+<?php
+    // todo; make install-$pluginname
+?>
+
+clean:
+    # remove everythin
+
+.vim:
+    # recreate the symlinks
+
+.vimrc:
+    # recreate the symlinks
